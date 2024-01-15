828edb517a17a5a2cc9dbbfc4f6b5cd89e700afc
krzysztoffila <umowy.fila@gmail.com>
2024-01-15 20:59:33 +0100

    Change array with comments to object

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 'use strict'
 const message = document.querySelector('.message');
-const messages = ['🟥 No Number!', '🎉 Correct Number!', '📈Too high!', '📉Too low!', '💥You lost the game!']
-
+const messages = {
+    noNumber: '🟥 No Number!',
+    win: '🎉 Correct Number!',
+    highNumber: '📈Too high!',
+    lowNumber: '📉Too low!',
+    gameOver: '💥You lost the game!'
+}
 const highscore = document.querySelector(".highscore")
 
 const number = document.querySelector('.number');
@@ -17,28 +22,28 @@ const secretNumber = Math.floor(Math.random() * 20) + 1;
 function checkNumber() {
     const guess = Number(document.querySelector('.guess').value);
     if (!guess) {
-        message.textContent = messages[0]
+        message.textContent = messages.noNumber
     } else if (guess === secretNumber) {
-        message.textContent = messages[1]
+        message.textContent = messages.win
         number.textContent = secretNumber;
         document.body.style.backgroundColor = "#60b347";
         highscore.textContent = scoreValue
     } else if (guess > secretNumber) {
         if (scoreValue > 1) {
-            message.textContent = messages[2];
+            message.textContent = messages.highNumber;
             scoreValue--;
             score.textContent = scoreValue;
         } else {
-            message.textContent = messages[4];
+            message.textContent = messages.gameOver;
             score.textContent = 0;
         }
     } else if (guess < secretNumber) {
         if (scoreValue > 1) {
-            message.textContent = messages[3];
+            message.textContent = messages.lowNumber;
             scoreValue--;
             score.textContent = scoreValue;
         } else {
-            message.textContent = messages[4];
+            message.textContent = messages.gameOver;
             score.textContent = 0;
         }
 
